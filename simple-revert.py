0ce039169abd46d6fa7d9ed5f5f887e0c5686ac2
Ilya Zverev <zverik@textual.ru>
2016-02-17 00:10:26 +0300

    Wow, it works

--- simple-revert.py
+++ simple-revert.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys, urllib2, getpass, base64
 from collections import defaultdict
+from copy import deepcopy
 from lxml import etree
 
 #API_ENDPOINT = 'https://api.openstreetmap.org/api/0.6'
@@ -8,6 +9,7 @@ API_ENDPOINT = 'http://master.apis.dev.openstreetmap.org/api/0.6'
 
 # Copied from http://stackoverflow.com/a/3884771/1297601
 class MethodRequest(urllib2.Request):
+  """A subclass to override Request method and content type."""
   GET = 'GET'
   POST = 'POST'
   PUT = 'PUT'
@@ -25,6 +27,7 @@ class MethodRequest(urllib2.Request):
     return urllib2.Request.get_method(self)
 
 def read_auth():
+  """Read login and password from keyboard, and prepare an basic auth header."""
   ok = False
   while not ok:
     login = raw_input('OSM Login: ')
@@ -41,13 +44,14 @@ def read_auth():
   return auth_header
 
 def obj_to_dict(obj):
-  """Converts XML object to a easy to use dict."""
+  """Converts XML object to an easy to use dict."""
   if obj is None:
     return None
   res = {}
   res['type'] = obj.tag
   res['id'] = int(obj.get('id'))
   res['version'] = int(obj.get('version'))
+  res['deleted'] = obj.get('visible') == 'false'
   if obj.tag == 'node' and 'lon' in obj.keys() and 'lat' in obj.keys():
     res['coord'] = (obj.get('lon'), obj.get('lat'))
   res['tags'] = { tag.get('k') : tag.get('v') for tag in obj.iterchildren('tag')}
@@ -57,55 +61,89 @@ def obj_to_dict(obj):
     res['refs'] = [(x.get('type'), x.get('ref'), x.get('role')) for x in obj.iterchildren('member')]
   return res
 
-def xml_to_diff(obj, obj_prev):
-  """Takes two XML trees and produces a diff."""
-  diff = {}
-  if obj_prev is None:
-    if obj is None:
+def dict_to_obj(obj):
+  """Converts object dict back to an XML element."""
+  if obj is None:
+    return None
+  res = etree.Element(obj['type'], {'id': str(obj['id']), 'version': str(obj['version'])})
+  res.set('visible', 'false' if obj['deleted'] else 'true')
+  if 'coord' in obj:
+    res.set('lon', obj['coord'][0])
+    res.set('lat', obj['coord'][1])
+  for k, v in obj['tags'].iteritems():
+    res.append(etree.Element('tag', {'k': k, 'v': v}))
+  if obj['type'] == 'way':
+    for nd in obj['refs']:
+      res.append(etree.Element('nd', {'ref': nd}))
+  elif obj['type'] == 'relation':
+    for member in obj['refs']:
+      res.append(etree.Element('member', {'type': member[0], 'ref': member[1], 'role': member[2]}))
+  return res
+
+def make_diff(obj, obj_prev):
+  """Takes two object dicts and produces a diff."""
+  diff = [('version', obj['version'])]
+  if obj_prev is None or obj_prev['deleted']:
+    if obj['deleted']:
       return diff
     else:
-      diff['action'] = 'delete'
+      diff.append(('create', obj))
+  elif obj['deleted']:
+    diff.append(('delete', obj_prev))
   else:
+    # Both objects are present, compare them
     # Moving nodes back
     if 'coords' in obj_prev:
-      if obj is None or obj['coords'] != obj_prev['coords']:
-        diff['move'] = { 'from': obj['coords'], 'to': obj_prev['coords'] }
+      if obj['coords'] != obj_prev['coords']:
+        diff.append(('move', obj_prev['coords'], obj['coords']))
 
     # Restoring old tags
-    if obj is None:
-      for k in obj_prev['tags']:
-        tdiff.append((k, obj_prev['tags'][k]))
-    else:
-      tdiff = []
-      for k in obj['tags']:
-        if k in obj_prev['tags'] and obj_prev['tags'][k] != obj['tags'][k]:
-          tdiff.append((k, obj_prev['tags'][k]))
-        elif k not in obj_prev['tags']:
-          tdiff.append((k, None))
-      for k in obj_prev['tags']:
-        if k not in obj['tags']:
-          tdiff.append((k, obj_prev['tags'][k]))
-    if tdiff:
-      diff['tags'] = tdiff
+    for k in obj['tags']:
+      if k in obj_prev['tags'] and obj_prev['tags'][k] != obj['tags'][k]:
+        diff.append(('tag', k, obj_prev['tags'][k], obj['tags'][k]))
+      elif k not in obj_prev['tags']:
+        diff.append(('tag', k, None, obj['tags'][k]))
+    for k in obj_prev['tags']:
+      if k not in obj['tags']:
+        diff.append(('tag', k, obj_prev['tags'][k], None))
 
     # Keeping references for ways and relations
-    refs = None
-    if obj_prev['type'] in ('way', 'relation'):
-      if obj is None:
-        ref = obj_prev['refs']
-      else:
-        if obj_prev['refs'] != obj['refs']:
-          raise Exception('Members for {0} {1} were changed, cannot roll that back'.format(obj['type'], obj['id']))
-    if refs:
-      diff['refs'] = refs
+    if 'refs' in obj and obj_prev['refs'] != obj['refs']:
+      raise Exception('Members for {0} {1} were changed, cannot roll that back'.format(obj['type'], obj['id']))
 
   return diff
 
-def diff_to_xml(diff, obj_last):
-  """Takes a diff and the last version of the object, and produces an initial object xml from it.
-  Changeset version is coded as $CHANGESET$."""
-  # TODO
-  return ''
+def merge_diffs(diff, diff_newer):
+  """Merge two sequential diffs."""
+  raise Exception('Merging diffs is not supported yet')
+
+def apply_diff(diff, obj):
+  """Takes a diff and the last version of the object, and produces an initial object from it."""
+  for change in diff:
+    if change[0] == 'version':
+      dver = change[1]
+    elif change[0] == 'move':
+      if 'coords' not in obj:
+        raise Exception('Move action found for {0} {1}'.format(obj['type'], obj['id']))
+      # If an object was moved after the last change, keep the coordinates
+      if dver == obj['version'] or change[2] == obj['coords']:
+        obj['coords'] = change[1]
+    elif change[0] == 'tag':
+      if change[1] in obj['tags']:
+        if change[3] is None:
+          pass # Somebody has already restored the tag
+        elif obj['tags'][change[1]] == change[3]:
+          if change[2] is None:
+            del obj['tags'][change[1]]
+          else:
+            obj['tags'][change[1]] = change[2]
+      else:
+        # If a modified tag was deleted after, do not restore it
+        if change[3] is None:
+          obj['tags'][change[1]] = change[2]
+    else:
+      raise Exception('Unknown or unprocessed by apply_diff change type: {0}'.format(change[0]))
+  return obj
 
 if __name__ == '__main__':
   if len(sys.argv) < 2:
@@ -113,8 +151,10 @@ if __name__ == '__main__':
     print 'Usage: {0} <changeset_id> [<changeset_id> ...]'.format(sys.argv[0])
     sys.exit(1)
 
-  opener = urllib2.build_opener(urllib2.HTTPSHandler(debuglevel=1))
+  debuglevel = 0
+  opener = urllib2.build_opener(urllib2.HTTPSHandler(debuglevel=debuglevel), urllib2.HTTPHandler(debuglevel=debuglevel))
   changesets = [int(x) for x in sys.argv[1:]]
+  ch_users = {}
   diffs = defaultdict(dict)
 
   for changeset_id in changesets:
@@ -125,20 +165,16 @@ if __name__ == '__main__':
       # Iterate over each object, download previous version (unless it's creation) and make a diff 
       for action in root:
         for obj_xml in action:
+          if changeset_id not in ch_users:
+            ch_users[changeset_id] = obj_xml.get('user')
           obj = obj_to_dict(obj_xml)
           if obj['version'] > 1:
             # Download the previous version
-            try:
-              response = opener.open('{0}/{1}/{2}/{3}'.format(API_ENDPOINT, obj['type'], obj['id'], obj['version'] - 1))
-              obj_prev = obj_to_dict(etree.parse(response).getroot()[0])
-            except urllib2.HTTPError as e:
-              if e.code == 410 or e.code == 404:
-                obj_prev = None
-              else:
-                raise e
+            response = opener.open('{0}/{1}/{2}/{3}'.format(API_ENDPOINT, obj['type'], obj['id'], obj['version'] - 1))
+            obj_prev = obj_to_dict(etree.parse(response).getroot()[0])
           else:
             obj_prev = None
-          diffs[(obj['type'], obj['id'])][obj['version']] = xml_to_diff(None if action.tag == 'delete' else obj, obj_prev)
+          diffs[(obj['type'], obj['id'])][obj['version']] = make_diff(obj, obj_prev)
     except Exception as e:
       print 'Failed to download changeset {0}: {1}'.format(changeset_id, e)
       import traceback
@@ -155,10 +191,7 @@ if __name__ == '__main__':
       raise Exception('Multiple versions of a same object ({0} {1}) are not supported yet.'.format(k[0], k[1]))
     diffs[k] = versions.values()[0]
 
-  # TODO: Sort diff, so deleted nodes are first
-  print diffs
-
-  xml = []
+  changes = []
   for obj, change in diffs.iteritems():
     try:
       # Download the latest version of an object
@@ -166,24 +199,55 @@ if __name__ == '__main__':
         response = opener.open('{0}/{1}/{2}'.format(API_ENDPOINT, obj[0], obj[1]))
         obj = obj_to_dict(etree.parse(response).getroot()[0])
       except urllib2.HTTPError as e:
-        if e.code == 410 or e.code == 404:
-          obj = None
+        if e.code == 410:
+          # Read the full history to get the latest version
+          response = opener.open('{0}/{1}/{2}/history'.format(API_ENDPOINT, obj[0], obj[1]))
+          obj = obj_to_dict(etree.parse(response).getroot()[-1])
         else:
           raise e
-      xml.append(diff_to_xml(change, obj))
+
+      # Apply the change
+      if len(change) == 2 and change[1] == 'create':
+        obj_new = { 'type': obj['type'], 'id': obj['id'], 'version': obj['version'] }
+        changes.append(('delete', obj_new))
+      elif len(change) == 2 and change[1] == 'delete':
+        # Restore only if the object is still absent
+        if obj['deleted']:
+          obj_new = change[1][1]
+          obj_new['version'] = obj['version']
+          changes.append(('modify', obj_new))
+      else:
+        obj_new = apply_diff(change, deepcopy(obj))
+        if obj_new != obj:
+          changes.append(('modify', obj_new))
     except Exception as e:
       print 'Failed to download the latest version of {0} {1}: {2}'.format(obj[0], obj[1], e)
       sys.exit(2)
 
+  if not changes:
+    print 'No changes to upload.'
+    sys.exit(0)
+
+  # Sort changes, so created nodes are first, and deleted are last
+  def change_as_key(ch):
+    act = ['create', 'modify', 'delete'].index(ch[0])
+    typ = ['node', 'way', 'relation'].index(ch[1]['type'])
+    if act == 2:
+      typ = 2 - typ
+    return '{0}{1}{2}'.format(act, typ, ch[1]['id'])
+
+  changes.sort(key=change_as_key)
+
+  # Now we need the OSM credentials
   auth_header = read_auth()
   opener.addheaders = [('Authorization', auth_header)]
 
   # Create changeset
-  create_xml = """<osm><changeset>
-  <tag k="created_by" v="simple-revert.py" />
-  <tag k="comment" v="Reverting {0}" />
-  </changeset></osm>""".format(', '.join([str(x) for x in changesets]))
-  request = MethodRequest(API_ENDPOINT + '/changeset/create', create_xml, method=MethodRequest.PUT)
+  create_xml = etree.Element('osm')
+  ch = etree.SubElement(create_xml, 'changeset')
+  ch.set('created_by', 'simple-revert.py')
+  ch.set('comment', 'Reverting {0}'.format(', '.join(['{0} by {1}'.format(str(x), ch_users[x]) for x in changesets])))
+  request = MethodRequest(API_ENDPOINT + '/changeset/create', etree.tostring(create_xml), method=MethodRequest.PUT)
   try:
     changeset_id = int(opener.open(request).read())
     print 'Writing to changeset {0}'.format(changeset_id)
@@ -191,8 +255,15 @@ if __name__ == '__main__':
     print 'Failed to create changeset', e
     sys.exit(0)
 
-  diff_xml = ''.join(['<osmChange version="0.6">'] + [x.replace('$CHANGESET$', str(changeset_id)) for x in xml] + ['</osmChange>'])
-  request = MethodRequest('{0}/changeset/{1}/upload'.format(API_ENDPOINT, changeset_id), diff_xml, method=MethodRequest.POST)
+  # Produce osmChange XML and upload it
+  osc = etree.Element('osmChange', {'version': '0.6'})
+  for c in changes:
+    act = etree.SubElement(osc, c[0])
+    el = dict_to_obj(c[1])
+    el.set('changeset', str(changeset_id))
+    act.append(el)
+
+  request = MethodRequest('{0}/changeset/{1}/upload'.format(API_ENDPOINT, changeset_id), etree.tostring(osc), method=MethodRequest.POST)
   try:
     response = opener.open(request)
   except Exception as e:
